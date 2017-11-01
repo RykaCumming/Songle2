07644fd5aa90014dce92fa0f704db40d530b7616
Ryka Cumming <RykaCumming@gmail.com>
2017-11-01 19:44:00 +0000

    01/11/17

--- app/src/main/java/uk/ac/ed/inf/songle2/NetworkActivity.java
+++ app/src/main/java/uk/ac/ed/inf/songle2/NetworkActivity.java
@@ -99,15 +99,19 @@ public class NetworkActivity extends FragmentActivity implements DownloadCallbac
 
     @Override
     public void updateFromDownload(String result) throws UnsupportedEncodingException,XmlPullParserException  {
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        startActivity(intent);
+    /*
         SongleXmlParser songleXmlParser =new SongleXmlParser();
         InputStream stream = new ByteArrayInputStream(result.getBytes(StandardCharsets.UTF_8.name()));
         try {
-            ArrayList<SongleXmlParser.Entry> entrys = (ArrayList<SongleXmlParser.Entry>) songleXmlParser.parse(stream);
-            Log.e("",entrys.get(0).artist);
+            songleXmlParser.parse(stream);
+//            Log.e("",songleXmlParser.parse(stream).get(0).artist);
         } catch (IOException e) {
             e.printStackTrace();
         }
         Log.e("NetworkActivity",result);
+        */
     }
 
     @Override
---
+++ app/src/main/java/uk/ac/ed/inf/songle2/ScrollingActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.ed.inf.songle2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling2);
+    }
+}
